e334c9e4f2cbf166476632a466162aece7e75df1
Kenneth Rodríguez <105812356+Kenneth506CR@users.noreply.github.com>
2023-11-17 08:32:10 -0600

    solo faltan los roles

--- script.js
+++ script.js
@@ -1,8 +1,155 @@
-function toggleMenu() {
+(function () {
+  // Función para mostrar/ocultar el menú
+  function toggleMenu() {
     var menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+  }
+
+  // Función para manejar el envío del formulario de inicio de sesión
+  function handleLogin(event) {
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+    var username = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+
+    if (
+      (username === "Kenneth506" || username === "Gnu") &&
+      password === "linux"
+    ) {
+      window.location.href = "index.html";
     } else {
-        menu.style.display = "block";
+      alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
     }
-}
+
+    // Llamar a la función para mostrar u ocultar el enlace después de la redirección
+    toggleCitasLink(username);
+  }
+
+  function toggleCitasLink(username) {
+    var citasLink = document.getElementById("citasLink");
+    if (username === "Kenneth506") {
+      citasLink.style.display = "block";
+    } else {
+      citasLink.style.display = "none";
+    }
+  }
+
+  // Función para manejar el envío del formulario de contacto
+  function handleContactForm(event) {
+    event.preventDefault();
+
+    // Recoger los valores del formulario
+    var nombreApellido = document.getElementById("nombreapellido").value;
+    var correoElectronico = document.getElementById("correoelectronico").value;
+    var telefono = document.getElementById("telefono").value;
+    var mensaje = document.getElementById("mensaje").value;
+    var contactoPreferido = document.querySelector(
+      'input[name="contacto"]:checked'
+    ).value;
+    var horarioPreferido = document.querySelector("select").value;
+    var recibirNovedades = document.querySelector(
+      'input[type="checkbox"]'
+    ).checked;
+
+    // Crear un objeto con los datos
+    var formData = {
+      nombreApellido: nombreApellido,
+      correoElectronico: correoElectronico,
+      telefono: telefono,
+      mensaje: mensaje,
+      contactoPreferido: contactoPreferido,
+      horarioPreferido: horarioPreferido,
+      recibirNovedades: recibirNovedades,
+    };
+
+    // Obtener los datos almacenados actualmente (si existen)
+    var storedData = localStorage.getItem("formData");
+
+    // Verificar si hay datos almacenados previamente
+    if (storedData) {
+      // Convertir la cadena JSON a un objeto
+      var storedArray = JSON.parse(storedData);
+
+      // Agregar el nuevo formulario al arreglo existente
+      storedArray.push(formData);
+
+      // Almacenar el arreglo actualizado en el almacenamiento local
+      localStorage.setItem("formData", JSON.stringify(storedArray));
+    } else {
+      // Si no hay datos almacenados, crear un nuevo arreglo con el primer objeto
+      localStorage.setItem("formData", JSON.stringify([formData]));
+    }
+
+    // Mostrar los valores en una alerta (puedes hacer lo que quieras con ellos aquí)
+    alert(
+      "Nombre y Apellido: " +
+        nombreApellido +
+        "\nCorreo Electrónico: " +
+        correoElectronico +
+        "\nTeléfono: " +
+        telefono +
+        "\nMensaje: " +
+        mensaje +
+        "\nContacto Preferido: " +
+        contactoPreferido +
+        "\nHorario Preferido: " +
+        horarioPreferido +
+        "\nRecibir Novedades: " +
+        recibirNovedades
+    );
+
+    // Llamar a la función para mostrar los datos después de manejar el formulario
+    displayFormData();
+  }
+
+  // Función para mostrar los datos almacenados
+  function displayFormData() {
+    var storedData = localStorage.getItem("formData");
+
+    if (storedData) {
+      var formDataArray = JSON.parse(storedData);
+      var formDataList = document.getElementById("formDataList");
+
+      formDataList.innerHTML = ""; // Limpiar la lista antes de agregar los elementos
+
+      formDataArray.forEach(function (formData, index) {
+        var listItem = document.createElement("li");
+        listItem.innerHTML = `
+          <strong>Cita  ${index + 1}:</strong>
+          <p>Nombre y Apellido: ${formData.nombreApellido}</p>
+          <p>Correo Electrónico: ${formData.correoElectronico}</p>
+          <p>Teléfono: ${formData.telefono}</p>
+          <p>Mensaje: ${formData.mensaje}</p>
+          <p>Contacto Preferido: ${formData.contactoPreferido}</p>
+          <p>Horario Preferido: ${formData.horarioPreferido}</p>
+          <p>Recibir Novedades: ${formData.recibirNovedades ? "Sí" : "No"}</p>
+        `;
+        formDataList.appendChild(listItem);
+      });
+    }
+  }
+
+  // Añadir manejadores de eventos después de que el DOM esté completamente cargado
+  document.addEventListener("DOMContentLoaded", function () {
+    // Manejador de evento para el botón de menú
+    var menuButton = document.getElementById("menuButton");
+    if (menuButton) {
+      menuButton.addEventListener("click", toggleMenu);
+    }
+
+    // Manejador de evento para el formulario de inicio de sesión
+    var loginForm = document.getElementById("loginForm");
+    if (loginForm) {
+      loginForm.addEventListener("submit", handleLogin);
+    }
+
+    // Manejador de evento para el formulario de contacto
+    var contactForm = document.getElementById("contactForm");
+    if (contactForm) {
+      contactForm.addEventListener("submit", handleContactForm);
+    }
+
+    // Llamar a la función para mostrar los datos al cargar la página
+    displayFormData();
+  });
+})();
